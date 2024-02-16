71c7f5e1ac8591f66f53e9d69b3dd29e5107f933
chethana-dissanayaka <cchethana173@gmail.com>
2024-02-16 15:07:38 +0530

    Cat function added

--- animal.py
+++ animal.py
@@ -1,2 +1,19 @@
-Hello
+import sys
+
+def cat():
+    print('Meow')
+
+def default():
+    print('Hello')
+
+def main():
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
+
+if __name__ == '__main__':
+    main()
+
+
 
